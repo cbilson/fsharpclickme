8e2a393719e118b3ff80170566a6701889fdbd16
Chris Bilson <cbilson@pobox.com>
2009-01-19 08:35:18 -0800

    initial commit

---
+++ App.fs
@@ -0,0 +1,13 @@
+﻿#light
+
+namespace FSharpClickMe
+
+    open System
+    open System.Windows
+    open System.Windows.Controls
+
+    type App() as this =
+        inherit Application()
+        
+        do
+            this.Startup.Add(fun _ -> this.RootVisual <- new Page())
---
+++ Page.fs
@@ -0,0 +1,19 @@
+﻿#light
+
+namespace FSharpClickMe
+
+    open System
+    open System.Windows
+    open System.Windows.Controls
+
+    type Page() as this =
+        inherit UserControl()
+        
+        do
+            let btn = new Button()
+            btn.HorizontalAlignment <- HorizontalAlignment.Center
+            btn.VerticalAlignment <- VerticalAlignment.Center
+            btn.Content <- "Click Me!"
+            btn.Click.Add(fun _ -> btn.Content <- "Clicked")
+            
+            this.Content <- btn
